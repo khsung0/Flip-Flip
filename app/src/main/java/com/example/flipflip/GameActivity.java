faefe60b3de100ec612b3d62a1920464eede6e72
김희성 <khsung0@nate.com>
2022-12-08 00:57:47 +0900

    Game Activity 까지 데이터 전달 구현

--- app/src/main/java/com/example/flipflip/GameActivity.java
+++ app/src/main/java/com/example/flipflip/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.flipflip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Intent intent = getIntent();
+        int player = intent.getIntExtra("player",0);
+        int difficulty = intent.getIntExtra("difficulty",0);
+        System.out.println("game Activity "+player+"  "+difficulty);
     }
 }
