983ef61c71f6b8cf62ef90541155b97ecfc00e45
김희성 <khsung0@nate.com>
2022-12-26 23:35:09 +0900

    Lombok과 카드 객체 세팅

---
+++ app/src/main/java/com/example/flipflip/pictureImg.java
@@ -0,0 +1,13 @@
+package com.example.flipflip;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class pictureImg {
+    boolean isOpend;    //카드 open 됐는지 확인
+    int imgIndex;       //이미지 인덱스
+}
